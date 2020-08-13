82c807cdba1d1b99a82a8c02e0c70ef3a7761d71
BayramGuenes <Bayram.Guenes@googlemail.com>
2020-08-13 17:54:51 +0200

    Änderung von 13.8.2020

--- loggerClassProxyImpl.go
+++ loggerClassProxyImpl.go
@@ -17,13 +17,21 @@ func (lcp LoggerClassProxy) StartLogTransaction(iInput InputParamStartTransact) 
 	lData, err := json.Marshal(iInput)
 	if err != nil {
 		eOutput.Exception.Occured = true
-		eOutput.Exception.ErrTxt = err.Error()
+		eOutput.Exception.ErrTxt = "json.Marshall.Input:" + err.Error()
 		return
 	}
-	println("LogServer.NameLogServer, LogServer.PortLogServer:"+LogServer.NameLogServer, LogServer.PortLogServer)
-	_, _, _, lExcep := timHTTP.SendPostMsg(LogServer.NameLogServer, LogServer.PortLogServer, "/StartTransaction", lData)
+	//println("LogServer.NameLogServer, LogServer.PortLogServer:"+LogServer.NameLogServer, LogServer.PortLogServer)
+	lResultArrByte, _, _, lExcep := timHTTP.SendPostMsg(LogServer.NameLogServer, LogServer.PortLogServer, "/StartTransaction", lData)
 	if lExcep.Occured {
-
+		eOutput.Exception.Occured = true
+		eOutput.Exception.ErrTxt = "StartTransaction SendPostMessage to tim_serv_logger:" + err.Error()
+		return
+	}
+	err = json.Unmarshal(lResultArrByte, &eOutput)
+	if err != nil {
+		eOutput.Exception.Occured = true
+		eOutput.Exception.ErrTxt = "json.Unmarshall.lResultArrByte:" + err.Error()
+		return
 	}
 	return
 }
@@ -37,9 +45,17 @@ func (lcp LoggerClassProxy) LogTransStep(iInput InputParamLogStep) (eException E
 		eException.ErrTxt = err.Error()
 		return
 	}
-	_, _, _, lExcep := timHTTP.SendPostMsg(LogServer.NameLogServer, LogServer.PortLogServer, "/LogTransactionStep", lData)
+	lResultArrByte, _, _, lExcep := timHTTP.SendPostMsg(LogServer.NameLogServer, LogServer.PortLogServer, "/LogTransactionStep", lData)
 	if lExcep.Occured {
-
+		eException.Occured = true
+		eException.ErrTxt = "LogTransStep SendPostMessage to tim_serv_logger:" + err.Error()
+		return
+	}
+	err = json.Unmarshal(lResultArrByte, &eException)
+	if err != nil {
+		eException.Occured = true
+		eException.ErrTxt = "json.Unmarshall.lResultArrByte:" + err.Error()
+		return
 	}
 	return
 }
@@ -53,9 +69,17 @@ func (lcp LoggerClassProxy) LogTransStepResult(iInput InputParamLogStepResult) (
 		eException.ErrTxt = err.Error()
 		return
 	}
-	_, _, _, lExcep := timHTTP.SendPostMsg(LogServer.NameLogServer, LogServer.PortLogServer, "/LogTransactionStepResult", lData)
+	lResultArrByte, _, _, lExcep := timHTTP.SendPostMsg(LogServer.NameLogServer, LogServer.PortLogServer, "/LogTransactionStepResult", lData)
 	if lExcep.Occured {
-
+		eException.Occured = true
+		eException.ErrTxt = "LogTransStepResult SendPostMessage to tim_serv_logger:" + err.Error()
+		return
+	}
+	err = json.Unmarshal(lResultArrByte, &eException)
+	if err != nil {
+		eException.Occured = true
+		eException.ErrTxt = "json.Unmarshall.lResultArrByte:" + err.Error()
+		return
 	}
 	return
 }
@@ -69,9 +93,17 @@ func (lcp LoggerClassProxy) FinishLogTransaction(iInput InputParamFinishTransact
 		eException.ErrTxt = err.Error()
 		return
 	}
-	_, _, _, lExcep := timHTTP.SendPostMsg(LogServer.NameLogServer, LogServer.PortLogServer, "/FinishLogTransaction", lData)
+	lResultArrByte, _, _, lExcep := timHTTP.SendPostMsg(LogServer.NameLogServer, LogServer.PortLogServer, "/FinishLogTransaction", lData)
 	if lExcep.Occured {
-
+		eException.Occured = true
+		eException.ErrTxt = "FinishLogTransaction SendPostMessage to tim_serv_logger:" + err.Error()
+		return
+	}
+	err = json.Unmarshal(lResultArrByte, &eException)
+	if err != nil {
+		eException.Occured = true
+		eException.ErrTxt = "json.Unmarshall.lResultArrByte:" + err.Error()
+		return
 	}
 	return
 }
