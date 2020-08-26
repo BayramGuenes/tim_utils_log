7edbc1cc9271d61e29c2c7daaf2ae66fa62e9ae0
BayramGuenes <Bayram.Guenes@googlemail.com>
2020-08-26 09:39:56 +0200

    Änderung von 26.8.2020

--- utils.go
+++ utils.go
@@ -9,5 +9,5 @@ type logWriter struct {
 }
 
 func (writer logWriter) Write(bytes []byte) (int, error) {
-	return fmt.Print(time.Now().UTC().Format("2006-01-02T15:04:05.999Z") + " [DEBUG] " + string(bytes))
+	return fmt.Print(time.Now().UTC().Format("2006-01-02  15:04:05") + " [DEBUG] " + string(bytes))
 }
